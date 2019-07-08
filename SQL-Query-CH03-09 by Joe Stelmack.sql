bb712468a50229d000764d9b942a00b81d29e3a8
Joseph <52648203+zero22s@users.noreply.github.com>
2019-07-08 13:34:01 -0500

    Add files via upload

---
+++ SQL-Query-CH03-09 by Joe Stelmack.sql
@@ -0,0 +1,82 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 08, 2019 at 08:28 PM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.3.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `wp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `PROJECT`
+--
+
+CREATE TABLE `project` (
+  `ProjectID` int(11) NOT NULL,
+  `ProjectName` char(50) NOT NULL,
+  `Department` char(35) NOT NULL,
+  `MaxHours` decimal(8,2) NOT NULL DEFAULT 100.00,
+  `StartDate` date DEFAULT NULL,
+  `EndDate` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `PROJECT`
+--
+
+INSERT INTO `PROJECT` (`ProjectID`, `ProjectName`, `Department`, `MaxHours`, `StartDate`, `EndDate`) VALUES
+(1300, '2017 Q3 Tax Preparation', 'Accounting', '145.00', '2017-08-10', '2017-10-15'),
+(1600, '2017 Q4 Portfolio Analysis', 'Finance', '140.00', '2017-10-05', NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `PROJECT`
+--
+ALTER TABLE `PROJECT`
+  ADD PRIMARY KEY (`ProjectID`),
+  ADD KEY `PROJ_DEPART_FK` (`Department`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `PROJECT`
+--
+ALTER TABLE `PROJECT`
+  MODIFY `ProjectID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1601;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `PROJECT`
+--
+ALTER TABLE `PROJECT`
+  ADD CONSTRAINT `PROJ_DEPART_FK` FOREIGN KEY (`Department`) REFERENCES `department` (`DepartmentName`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
